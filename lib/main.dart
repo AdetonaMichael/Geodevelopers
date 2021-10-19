a85bd4fa25bfa0cf0293039f0dec6d0130ace6af
AdetonaMichael <powerofelectron616@gmail.com>
2021-10-19 18:13:09 +0100

    added all files to remote repo

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// this is a simple code to demonstrate my knowledge of dart
+import 'package:flutter/material.dart';
+
+// main method begins execution of the application
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+      title:  Center(child: Text("Geo Developers"),
+      ),
+    ),
+    ),
+    ),
+  );
+}
