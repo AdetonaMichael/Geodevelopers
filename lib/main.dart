14e9e13a46e11cdf720abf61ecc14883203ec014
AdetonaMichael <powerofelectron616@gmail.com>
2021-11-09 06:33:41 +0100

    Second day playing with Dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -36,48 +36,107 @@
 
 // ignore_for_file: prefer_const_constructors
 
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(GeoNerds());
+// }
+//
+// // ignore: use_key_in_widget_constructors
+// class GeoNerds extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue[900],
+//           title: Center(
+//             child: Text("GEO NERD"),
+//           ),
+//         ),
+//         body: SafeArea(
+//               child:Column(children: [
+//             Container(
+//             child: Image(
+//               image: AssetImage('images/sat.jpg'),
+//             ),
+//           ),
+//           Container(
+//             child: Center(child: Text("Ever Wanted Your Personal Eye in The Sky? ",
+//                           style: TextStyle(fontWeight:FontWeight.bold),
+//                           ),
+//             ),
+//           ),
+//               ],)
+//
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           backgroundColor: Colors.blue[900],
+//           onPressed: () {},
+//           child: Icon(Icons.home),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//creating a new flutter project to test all the shit
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(GeoNerds());
+  runApp(
+    GeoNerd()
+    );
 }
 
-// ignore: use_key_in_widget_constructors
-class GeoNerds extends StatelessWidget {
+class GeoNerd  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Center(
-            child: Text("GEO NERD"),
-          ),
-        ),
-        body: SafeArea(
-              child:Column(children: [      
-            Container(
-            child: Image(
-              image: AssetImage('images/sat.jpg'),
+    return   MaterialApp(
+    home: Scaffold(
+    appBar: AppBar(
+backgroundColor: Colors.blue[900],
+title: Center(
+child: Text("GEO NERD"),
+),
+),
+ body: SafeArea(
+       child: Column(
+         children: [
+            Container(child: Image(image: AssetImage('images/sat.jpg'),
             ),
-          ),
-          Container(
-            child: Center(child: Text("Ever Wanted Your Personal Eye in The Sky? ",
-                          style: TextStyle(fontWeight:FontWeight.bold),
-                          ),
             ),
-          ),
-              ],)
-        
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue[900],
-          onPressed: () {},
-          child: Icon(Icons.home),
-        ),
-      ),
-    );
+           Container(child: Center(child: Text("Welcome To GeoNerd, Your personal eye in the sky",
+           style: TextStyle(
+             fontSize:20.0,
+             fontWeight: FontWeight.bold,
+
+           )),
+           ),
+           ),
+         ],
+       ),
+ ),
+      floatingActionButton: FloatingActionButton(onPressed: (){},
+      backgroundColor:Colors.blue[900],
+      child: Icon(Icons.home),),
+      drawer: Drawer(
+        child: Column(
+          children: const [
+            CircleAvatar(
+              radius:50.0,
+              backgroundImage: AssetImage('images/bat2.png'),
+            ),
+            Text('Michael Adetona',
+            style: TextStyle(
+                 fontSize: 40.0,
+                 fontWeight: FontWeight.bold
+            ),
+            )
+          ],
+        )
+      )
+),
+);
   }
 }
-
-
